7bf82215f1a808e820f384052fa6e4e5e4c74f1c
ddeblander <david.deblander@condorcet.be>
2022-12-29 21:58:25 +0100

    base project with base Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ClasseComponent} from "./components/classe/classe.component";
+import {CoursComponent} from "./components/cours/cours.component";
+import {SalleComponent} from "./components/salle/salle.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'classe', component: ClasseComponent},
+  {path:'cours', component: CoursComponent},
+  {path:'salle', component: SalleComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClasseComponent } from './components/classe/classe.component';
+import { CoursComponent } from './components/cours/cours.component';
+import { SalleComponent } from './components/salle/salle.component';
+import {MainMenuComponent} from "./components/main-menu/main-menu.component";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClasseComponent,
+    CoursComponent,
+    SalleComponent,
+    MainMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

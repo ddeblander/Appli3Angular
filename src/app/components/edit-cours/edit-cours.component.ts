c6d14c420cb8fe35e36c9950d73538f7b42ceb41
ddeblander <david.deblander@condorcet.be>
2022-12-30 20:37:38 +0100

    created all base edit

---
+++ src/app/components/edit-cours/edit-cours.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ActivatedRoute} from "@angular/router";
+import {CoursService} from "../../services/cours.service";
+
+@Component({
+  selector: 'app-edit-cours',
+  templateUrl: './edit-cours.component.html',
+  styleUrls: ['./edit-cours.component.css']
+})
+export class EditCoursComponent
+{
+  coursFormGroup?: FormGroup;
+  submitted = false;
+  idCours:number;
+  constructor(private coursService: CoursService,private fb:
+    FormBuilder,activatedRoute : ActivatedRoute)
+  {
+    this.idCours= activatedRoute.snapshot.params['idCours'];
+  }
+  ngOnInit(): void {
+    this.coursService.getCours(this.idCours).subscribe(
+      cours =>{this.coursFormGroup = this.fb.group({
+        idCours: [cours.idcours, Validators.required],
+        code: [cours.code, Validators.required],
+        intitule: [cours.intitule, Validators.required],
+
+
+      })
+      });
+  }
+  onUpdateCours(): void {
+    this.submitted = true;
+    if (this.coursFormGroup?.invalid) { return; }
+    this.coursService.updateCours(this.coursFormGroup?.value.idCours,this.coursFormGroup?.value).subscribe();
+    // obligé d'envoyer l'ID puis Cours sinon l'ID est undifined
+  }
+
+}
